2a453a058951a36830c22d2adc011574767bca03
Yingying <92928939+yingyinghao@users.noreply.github.com>
2023-07-24 23:57:08 -0400

    update

--- server/server.js
+++ server/server.js
@@ -1,13 +1,60 @@
 const express = require('express');
-const spotifywebapi = require('spotify-web-api-node');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/refresh', (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: 'http://localhost:3000',
+    clientId: '69a151701abb4b7a8ca74139ede6e72a',
+    clientSecret: '2243910908384d4aa2253510cf742a15',
+    refreshToken,
+});
+
+
+spotifyApi.
+refreshAccessToken()
+.then(data => {
+  res.json({
+    accessToken: data.body.access_token,
+    expiresIn: data.body.expires_in,
+  });
+})
+.catch((err) => {
+  res.sendStatus(400);
+});
+});
+
+
 
 app.post('/login', (req, res) => {
-  const spotify = spotifywebapi({
+  const code = req.body.code;
+  const spotifyApi = new SpotifyWebApi({
     redirectUri: 'http://localhost:3000',
     clientId: '69a151701abb4b7a8ca74139ede6e72a',
     clientSecret: '2243910908384d4aa2253510cf742a15'
   });
 
+
+  spotifyApi
+  .authorizationCodeGrant(code)
+  .then(data => {
+    res.json({
+      accessToken: data.body.access_token,
+      refreshToken: data.body.refresh_token,
+      expiresIn: data.body.expires_in,
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+    res.sendStatus(400);
+  });
 });
+
+
+app.listen(3001);
